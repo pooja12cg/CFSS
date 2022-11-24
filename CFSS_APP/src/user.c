3d36271973213d3ccbecc8d3292a2e82423990e9
Shreoshi <shreoshi@shreoshivirtualbox>
2022-11-25 03:47:35 +0530

    updated

--- CFSS_APP/src/user.c
+++ CFSS_APP/src/user.c
@@ -1,8 +1,18 @@
 /*******************************************************************
- * FILE_NAME      :   user.c
- * DESCRIPTION    :   It takes the strings from user and search
- *                        
- * RETURN_TYPE    :   returns success or failure
+ ** FILE_NAME      :   user.c
+ **
+ ** DESCRIPTION    :   this file is used to execute user related function
+ **                        
+ **
+ ** Revision History :
+ ** Date		Name 		Reference 		Reason
+ **
+ ** 25-11-2022          Group04          New                  user related function
+ **
+ ** Copyright 2022,Altran Group All Rights Reserved
+ **
+ *************************************************************
+ ** RETURN_TYPE    :   returns success or failure
  ************************************************************/
 
 /***************************************************************
@@ -13,25 +23,26 @@
 
 
 /*******************************************************************
- * FUNCTION_NAME  :   main
- * DESCRIPTION    :   It takes the strings from user and search
- *                        
- * RETURN_TYPE    :   returns success or failure
+ ** FUNCTION_NAME  :  cfsActivation
+ **
+ ** DESCRIPTION    :   It helps to active the system
+ **                        
+ ** RETURN_TYPE    :   returns success or failure
  ************************************************************/
-void cfsActivation(CFSS *cfss, UPD *upd, int refID)
+void cfsActivation(CFSS *cfss, UPD *upd, int refID)/*fuction*/
 {
-	int cfsActive=0;
-	int cfsNumber=0;
-	int flag = 0;
-	int cfs =0;
+	int cfsActive=0;/*cfsActive is a interger, variable declaration*/
+	int cfsNumber=0;/*cfsNumber is a interger, variable declaration*/
+	int flag = 0;/*flag  a interger, variable declaration*/
+	int cfs =0;/*cfs is a interger type variable used to in swirch case*/
 
 	while(upd != NULL)
 	{
-		if(upd->_id == refID)
+		if(upd->_id == refID)/*refID value will be stored in this id*/
 		{
-			flag++;
+			flag++;/*flag value will increase*/
 			upd->regFlag = 1;
-			break;
+			break;/*break statement*/
 		}
 		upd = upd->next;
 	}
@@ -41,41 +52,44 @@ void cfsActivation(CFSS *cfss, UPD *upd, int refID)
 
 	while(cfss != NULL)
 	{
-		if(cfss->_id == refID)
+		if(cfss->_id == refID)/*refID value will be stored in this id*/
 		{
-			flag++;
+			flag++;/*flag value will increase*/
 			cfss->regFlag = 1;
-			break;
+			break;/*break statement*/
 		}
 		cfss = cfss->next;
 	}
 
-	printf("\n\tDo you want to activate Call Forwarding services(0/1) : ");
-	scanf("%d",&cfsActive);
-	if(cfsActive)
+	printf("\n\tDo you want to activate Call Forwarding services(0/1) : ");/*asking about activation*/
+	scanf("%d",&cfsActive);/*read that value*/
+	if(cfsActive)/*value will go through this if statement*/
 	{
 		flag=1;
 		cfss->cfsActive = 1;
-		printf("\n\tEnter the call forwarding number :");
-		scanf("%d",&cfsNumber);
+		printf("\n\tEnter the call forwarding number :");/*in which number call will be forwarded*/
+		scanf("%d",&cfsNumber);/*call forwarding number will be stored*/
 		cfss->cfsNumber = cfsNumber;
-		printf("\n\tPress \n\t1.Unconditional \n\t2.No Reply \n\t3.Busy ");
-		printf("\n\tChoice :");
-		scanf("%d",&cfs);
-		switch(cfs)
+		printf("\n\tPress \n\t1.Unconditional \n\t2.No Reply \n\t3.Busy ");/*types of call forwarding*/
+		printf("\n\tChoice :");/*choose the option*/
+		scanf("%d",&cfs);/*read the option*/
+		switch(cfs)/*switch case*/
 		{
 			case 1:
-				printf("\n\tYour Call has been forwarded to Unconditional type");
+				/*for uncontional type*/
+			/*	printf("\n\tYour Call has been forwarded to Unconditional type");*/
 				strcpy(cfss->status, "Unconditional");
 				printf("\n\tYour Call has been forwarded to Unconditional type");
 				break;
 			case 2:
+                               /*for No reply type*/
 				strcpy(cfss->status, "No Reply");
 				printf("\n\tYour Call has been forwarded to No Reply type");
 				
 				break;
 
 			case 3:
+				/*for Busy type*/
 				strcpy(cfss->status, "Busy");
 				printf("\n\tYour Call has been forwarded to Busy type");
 				break;
@@ -91,16 +105,17 @@ void cfsActivation(CFSS *cfss, UPD *upd, int refID)
 
 }
 /*******************************************************************
- * FUNCTION_NAME  :   main
- * DESCRIPTION    :   It takes the strings from user and search
- *                        
- * RETURN_TYPE    :   returns success or failure
+ **  FUNCTION_NAME  :   cfsDeactivation
+ ** 
+ **  DESCRIPTION    :   It helps to the system is deactive or not
+ **                        
+ **  RETURN_TYPE    :   returns success or failure
  ************************************************************/
-void cfsDeactivation(CFSS *cfss)
+void cfsDeactivation(CFSS *cfss)/*function */
 {
 	int cfsActive=0;
 
-	printf("\n\tDo you want to deactivate the call(0/1): ");
+	printf("\n\tDo you want to deactivate the call(0/1): ");/*for deactivation*/
 	scanf("%d",&cfsActive);
 	if(cfsActive==1)
 	{
@@ -116,12 +131,13 @@ void cfsDeactivation(CFSS *cfss)
 }
 
 /*******************************************************************
- * FUNCTION_NAME  :   main
- * DESCRIPTION    :   It takes the strings from user and search
- *                        
- * RETURN_TYPE    :   returns success or failure
+ ** FUNCTION_NAME  :  makecall 
+ **
+ ** DESCRIPTION    :  it help to enable the system
+ **                        
+ **  RETURN_TYPE    :   returns success or failure
  ************************************************************/
-void makeCall(UPD *hupd, CFSS *hcfss, int phno)
+void makeCall(UPD *hupd, CFSS *hcfss, int phno)/*make call function*/
 {
 	int refID = 0, flag =0;
 
